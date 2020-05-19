aeb85d396f2668ff23a04e65e6593edbc607de8a
Bruno Francisco <64901068+francisbruno@users.noreply.github.com>
2020-05-18 21:06:17 -0300

    Upload

---
+++ src/component/text_En.js
@@ -0,0 +1,6 @@
+export default [
+	{
+		id:'ABOUT', text: "Aren't just contradictions nor fictions Is not what you imagine, nor what you suppopse Isn't something you've ever felt, nor anything you've ever seen It is the depth you have never reached, nor even plunged It's the sensibility & vulnerability you call 'Drama' It is the Mystery: indescribable & invisible"
+
+	},
+]
